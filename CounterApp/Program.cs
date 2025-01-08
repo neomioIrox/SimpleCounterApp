ca74b49ff5f68069004ec33bc0cfc13d5a0969dc
neomioIrox <neomio@i-rox.net>
2025-01-08 09:23:46 +0200

    Initial commit

---
+++ CounterApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        string input;
+
+        Console.WriteLine("Welcome to the Counter App!");
+
+        do
+        {
+            Console.Write($"Counter: {counter} > ");
+            input = Console.ReadLine()?.Trim().ToLower();
+            counter++;
+            
+        } while (input != "exit");
+    }
+}
